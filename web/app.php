c114f37bf88f24a340071cfc4f63e47ff51bad54
Mell Zamora <mellzamora@outlook.com>
2013-08-13 10:04:57 +0800

    migration config for 2.3

--- web/app.php
+++ web/app.php
@@ -1,8 +1,5 @@
 <?php
-// if you want to use the SonataPageBundle with multisite
-// using different relative paths, you must change the request
-// object to use the SiteRequest
-// use Sonata\PageBundle\Request\SiteRequest as Request;
+
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -11,9 +8,10 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
-
+/*
 $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
+*/
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
