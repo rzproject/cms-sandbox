7599cb7316aef7908a862d29291eae993b8214c3
mellzamora <mellzamora@outlook.com>
2014-11-20 17:27:53 +0800

    fixed prod batch update issue

--- web/app.php
+++ web/app.php
@@ -24,6 +24,7 @@ require_once __DIR__.'/../app/AppCache.php';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
