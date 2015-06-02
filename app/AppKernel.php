87c7833d6dc2b43c315c9038ae7be42cbe004608
mellzamora <mellzamora@outlook.com>
2015-06-02 09:32:34 +0800

    updated AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,16 +16,8 @@ class AppKernel extends Kernel
      */
     public function __construct($environment, $debug)
     {
-        $this->environment = $environment;
-        $this->debug = (bool) $debug;
-        $this->rootDir = $this->getRootDir();
-        $this->name = $this->getName();
-
-        if ($this->debug) {
-            $this->startTime = microtime(true);
-        }
-
-        bcscale(3);
+	    bcscale(3);
+	    parent::__construct($environment, $debug);
     }
 
     protected function initializeContainer() {
